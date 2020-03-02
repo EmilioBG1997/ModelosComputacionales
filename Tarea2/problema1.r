0ea26a3226e77d07058b58d63b95d685d1c02b26
EmilioBG1997 <emilio.barrera1997@gmail.com>
2020-03-02 10:12:21 -0600

    agregados algunos problemas y modificado el documento

---
+++ Tarea2/problema1.r
@@ -0,0 +1,7 @@
+library(markovchain)
+P=matrix(c(1,0,0,0, .7,.2,.1,0, .5,.1,.2,.2, 0,0,0,1), nrow=4, byrow=TRUE)
+mc=new("markovchain",transitionMatrix=P,states=c('0','1','2','3'), name="Problema 1 - Deudores")
+
+print(mc) ##IMPRIME LA MATRIZ DE TRANSICION
+plot(mc) ##CREA EL DIAGRAMA DE TRANSICION
+absorptionProbabilities(mc) ##REGRESA LAS PROBABILIDADES DE ABSORCION DE CADA ESTADO
